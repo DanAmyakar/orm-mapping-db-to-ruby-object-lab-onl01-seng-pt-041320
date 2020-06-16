e3705981cd509960c08f5d7726c847351398a289
Daniel Amyakar <dan.rockit@gmail.com>
2020-06-16 20:30:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,7 +62,7 @@ class Student
       WHERE grade = 10 ORDER BY id LIMIT 1;
     SQL
     
-    DB[:conn].execute(sql).each{|row| self.new_from_db(row)}
+    DB[:conn].execute(sql).each{|row| self.new_from_db(row)}.pop
   end
   
   def self.all_students_in_grade_X(x)
