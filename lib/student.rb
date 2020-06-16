95fff7fb6d60039f903c82b21c660e83ae9b9802
Daniel Amyakar <dan.rockit@gmail.com>
2020-06-16 19:37:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,7 +49,7 @@ class Student
     # return a new instance of the Student class
     sql = <<-SQL
       SELECT * FROM students
-      WHERE grade < 12;
+      WHERE grade < "12";
     SQL
     
     DB[:conn].execute(sql).map{|row| self.new_from_db(row)}
